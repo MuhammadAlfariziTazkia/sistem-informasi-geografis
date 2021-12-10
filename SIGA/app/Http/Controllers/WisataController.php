8e377df2faecabe0c7c992085fe648d80ca94bfb
Muhammad Alfarizi Tazkia <alfatazkia@gmail.com>
2021-12-10 08:51:12 +0700

    [FEAT] add controller for wisata

--- SIGA/app/Http/Controllers/WisataController.php
+++ SIGA/app/Http/Controllers/WisataController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ObjectModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WisataController extends Controller
 {
@@ -14,26 +16,33 @@ class WisataController extends Controller
         // KALAU ADA SEARCH KEY QUERY DAN ADA SORTBY QUERY
         if ($search_key && $sortby_key){
             // STATEMENT
+            $wisata = DB::select(DB::raw("SELECT * FROM object WHERE nama ILIKE '%".$search_key."%' OR alamat ILIKE '%".$search_key."%' ORDER BY ".$sortby_key." DESC"));
         } 
         // KALAU ADA SEARCH KEY TAPI TIDAK ADA SORTBY KEY
         else if($search_key && !$sortby_key){
             // STATEMENT
+            $wisata = DB::select(DB::raw("SELECT * FROM object WHERE nama ILIKE '%".$search_key."%' OR alamat ILIKE '%".$search_key."%'"));
         }
         // KALAU TIDAK ADA SEARCH KEY TAPI ADA SORTBY KEY
         else if(!$search_key && $sortby_key){
             // STATEMENT
+            $wisata = ObjectModel::select('*')->orderBy($sortby_key, 'desc')->get();
         }
         // KALAU TIDAK ADA KEDUANYA
         else if(!$search_key && !$sortby_key) {
             // STATEMENT
+            $wisata = ObjectModel::all();
         }
         // SELAIN ITU ARAHKAN KE HALAMAN ERROR NOT FOUND 
         else{
             // STATEMENT
         }
+        return $wisata;
     }
 
-    public function detail($slug){
-        return $slug;
+    public function detail($id){
+        $wisata = ObjectModel::find($id);
+        
+        return $wisata;
     }
 }
