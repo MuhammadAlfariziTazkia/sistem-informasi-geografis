373a33cae55dd4b616a571e5a6910800a168678b
ryznocaHub <fikrihalim27@gmail.com>
2021-12-14 13:26:52 +0700

    Add Navbar component

---
+++ SIGA/resources/views/template/navbar.blade.php
@@ -0,0 +1,18 @@
+<ul class="nav-container">
+    <!-- Navigation item -->
+    <li>
+        <div class="logo d-flex justify-content-center align-items-center">
+            <img src="{{ asset('img/logo.png') }}" alt="" >
+        </div>
+    </li>
+
+    <!-- Navigation item that sticks to the right -->
+    <li class="nav__item--right">
+        <a href="">
+            <p>Wisata</p>
+        </a>
+        <a href="">
+            <p>Olahraga</p>
+        </a>
+    </li>
+</ul>
