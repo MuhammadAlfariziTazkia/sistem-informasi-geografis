15ff86426e37dc1e32eb2681f5d69f73fc477890
Muhammad Alfarizi Tazkia <alfatazkia@gmail.com>
2021-12-21 18:57:38 +0700

    [FEAT] add condition query and variable sortby

--- SIGA/app/Http/Controllers/WisataController.php
+++ SIGA/app/Http/Controllers/WisataController.php
@@ -13,8 +13,9 @@ class WisataController extends Controller
     //
     public function index(Request $request){
         $object_type = 'wisata';
-
+        
         $ip = $_SERVER['REMOTE_ADDR'];
+        // echo $ip;
         // $currentLocation = GeoIP::getLocation($ip);
         $currentLocation = GeoIP::getLocation('111.94.186.184');
         $myLatitude = $currentLocation['lat'];
@@ -22,40 +23,42 @@ class WisataController extends Controller
 
         $search_key = $request->query('search');
         $sortby_key = $request->query('sortby');
+        
+        $sortby = $sortby_key;
 
         // KALAU ADA SEARCH KEY QUERY DAN ADA SORTBY QUERY
         if ($search_key && $sortby_key){
             // STATEMENT
-            $data = DB::select(DB::raw("SELECT *, ST_Distance(ST_MakePoint(".$myLatitude.",". $myLongitude.")::geometry,geometry::geometry,true) as distance FROM object WHERE nama ILIKE '%".$search_key."%' OR alamat ILIKE '%".$search_key."%' ORDER BY ".$sortby_key." DESC"));
+            $data = DB::select(DB::raw("SELECT *, ST_Distance(ST_MakePoint(".$myLatitude.",". $myLongitude.")::geometry,geometry::geometry,true) as distance FROM object WHERE jenis = 'Pariwisata' AND (nama ILIKE '%".$search_key."%' OR alamat ILIKE '%".$search_key."%') ORDER BY ".$sortby_key." DESC"));
         } 
         // KALAU ADA SEARCH KEY TAPI TIDAK ADA SORTBY KEY
         else if($search_key && !$sortby_key){
             // STATEMENT
-            $data = DB::select(DB::raw("SELECT *, ST_Distance(ST_MakePoint(".$myLatitude.",". $myLongitude.")::geometry,geometry::geometry,true) as distance FROM object WHERE nama ILIKE '%".$search_key."%' OR alamat ILIKE '%".$search_key."%' ORDER BY distance DESC"));
+            $data = DB::select(DB::raw("SELECT *, ST_Distance(ST_MakePoint(".$myLatitude.",". $myLongitude.")::geometry,geometry::geometry,true) as distance FROM object WHERE jenis = 'Pariwisata' AND (nama ILIKE '%".$search_key."%' OR alamat ILIKE '%".$search_key."%') ORDER BY distance DESC"));
         }
         // KALAU TIDAK ADA SEARCH KEY TAPI ADA SORTBY KEY
         else if(!$search_key && $sortby_key){
             // STATEMENT
-            $data = DB::select(DB::raw("SELECT *, ST_Distance(ST_MakePoint(".$myLatitude.",". $myLongitude.")::geometry,geometry::geometry,true) as distance FROM object WHERE ORDER BY ".$sortby_key." DESC"));
+            $data = DB::select(DB::raw("SELECT *, ST_Distance(ST_MakePoint(".$myLatitude.",". $myLongitude.")::geometry,geometry::geometry,true) as distance FROM object WHERE jenis = 'Pariwisata' ORDER BY ".$sortby_key." DESC"));
         }
         // KALAU TIDAK ADA KEDUANYA
         else if(!$search_key && !$sortby_key) {
             // STATEMENT
-            $data = DB::select(DB::raw("SELECT *, ST_Distance(ST_MakePoint(".$myLatitude.",". $myLongitude.")::geometry,geometry::geometry,true) as distance FROM object ORDER BY distance DESC"));
+            $data = DB::select(DB::raw("SELECT *, ST_Distance(ST_MakePoint(".$myLatitude.",". $myLongitude.")::geometry,geometry::geometry,true) as distance FROM object WHERE jenis = 'Pariwisata' ORDER BY distance DESC"));
         }
         // SELAIN ITU ARAHKAN KE HALAMAN ERROR NOT FOUND 
         else{
             // STATEMENT
         }
 
-        return view("pages.search", compact('object_type', 'myLatitude', 'myLongitude', 'data'));
+        return compact('object_type', 'myLatitude', 'myLongitude', 'sortby', 'data');
     }
 
     public function detail($id){
         $object_type = 'wisata';
         $data = ObjectModel::find($id);
         $assets = DB::select(DB::raw("SELECT * FROM asset WHERE object_id = '".$id."'"));
-        $rekomendasi = DB::select(DB::raw("SELECT * FROM object ORDER BY rating DESC"));
+        $rekomendasi = DB::select(DB::raw("SELECT * FROM object ORDER BY rating DESC LIMIT 6"));
         return compact('object_type', 'data', 'assets', 'rekomendasi');
     }
 }
